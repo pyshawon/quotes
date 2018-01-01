8ca51cb8a97c1f2404dea3d644a60b354c302c62
Shaid Hasan Shawon <pyshawon@gmail.com>
2018-01-02 02:33:29 +0600

    added

--- quotable/settings.py
+++ quotable/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'quotable.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'quotes',
+#         'USER': 'root',
+#         'PASSWORD': 'xx',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'quotes',
-        'USER': 'root',
-        'PASSWORD': 'xx',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
